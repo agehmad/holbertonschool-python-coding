4e90630fad65851c939df7b410b6d8a77a4dce8f
Ahmad <asusahmad26@gmail.com>
2025-12-10 18:54:11 +0400

    1

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Docstring for python-classes.2-square
+"""
+
+
+class Square:
+    """
+    Docstring for Square
+    """
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        return self.__size**2
