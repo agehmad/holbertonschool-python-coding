e0728ec81191b4d2b3c703bd2d9c9c7c4538c64d
Ahmad <asusahmad26@gmail.com>
2025-12-10 18:45:22 +0400

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,7 +8,7 @@ class Square:
     """
     Docstring for Square
     """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if not type(size) is int:
             raise TypeError("size must be an integer")
         elif size < 0:
